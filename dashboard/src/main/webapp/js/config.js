612a45953add8457110dc310f18208bc25359729
Adrian Nieto <adrian@lcc.uma.es>
2014-11-13 12:16:40 +0100

    Added static SeaClouds Dashboard Concept to the project

---
+++ dashboard/src/main/webapp/js/config.js
@@ -0,0 +1,9 @@
+/*
+    Live demo configuration
+
+    - This file is dependant on where Brooklyn and Sla is mounted and needs to
+    be changed.
+*/
+
+var BROOKLYN_ENDPOINT = "http://192.168.24.128:8081";
+var SLADASHBOARD_ENDPOINT = "http://localhost:8082";
