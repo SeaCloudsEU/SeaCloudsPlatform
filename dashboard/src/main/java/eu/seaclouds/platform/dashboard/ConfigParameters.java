4d0a7462c9f65d5bd95a9c36d887d136ed193a4e
Miguel Barrientos <mbarrientos@lcc.uma.es>
2014-11-19 11:22:14 +0100

    Fixed error on parent pom.xml and cleaned deployment repositories

--- dashboard/src/main/java/eu/seaclouds/platform/dashboard/ConfigParameters.java
+++ dashboard/src/main/java/eu/seaclouds/platform/dashboard/ConfigParameters.java
@@ -4,7 +4,7 @@ package eu.seaclouds.platform.dashboard;
  * Created by Adrian on 14/11/2014.
  */
 public interface ConfigParameters {
-    public static String DEPLOYER_ENDPOINT = "http://devtest.scenic.uma.es:8081/";
-    public static String SLA_ENDPOINT = "http://localhost:8081";
 
+    public static String DEPLOYER_ENDPOINT = "http://localhost:8081/"; // CHANGE THIS
+    public static String SLA_ENDPOINT = "http://localhost:8082";       // CHANGE THIS
 }
