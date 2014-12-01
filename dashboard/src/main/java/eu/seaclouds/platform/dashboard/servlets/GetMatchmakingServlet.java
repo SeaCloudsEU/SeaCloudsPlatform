43f362e0e681e9b2989fbbca9d91333459dae9f7
Miguel Barrientos <mbarrientos@lcc.uma.es>
2014-12-01 23:16:31 +0100

    Added servlets for Planner & Matchmaker: 	- getInitialToscaPlan: returns the existing Nuro Tosca YAML plan used for the Review Demo. 	- getMatchMaking: returns result of the matchmaking process on the initial Tosca YAML Plan.

---
+++ dashboard/src/main/java/eu/seaclouds/platform/dashboard/servlets/GetMatchmakingServlet.java
@@ -0,0 +1,36 @@
+package eu.seaclouds.platform.dashboard.servlets;
+
+import com.google.api.client.util.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closeables;
+import seaclouds.Planner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @author mbarrientos
+ */
+public class GetMatchmakingServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        InputStream input = getClass().getResourceAsStream("/nuroCase.yaml");
+        if (input == null) {
+            response.sendError(500, "YAML File not found");
+        } else {
+            String nuroCaseYaml = CharStreams.toString(new InputStreamReader(input, Charsets.UTF_8));
+            Closeables.closeQuietly(input);
+            Planner planner = new Planner(nuroCaseYaml);
+
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(planner.plan());
+        }
+    }
+}
