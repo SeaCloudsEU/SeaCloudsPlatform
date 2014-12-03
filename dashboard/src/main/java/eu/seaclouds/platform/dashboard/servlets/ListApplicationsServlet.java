1c52a26ba25017e23ffc1a6e1225730cb6ecaa91
Adrian Nieto <adrian@lcc.uma.es>
2014-12-03 16:44:23 +0100

    Added Location of each application listed in ListApplicationSevrlet.

--- dashboard/src/main/java/eu/seaclouds/platform/dashboard/servlets/ListApplicationsServlet.java
+++ dashboard/src/main/java/eu/seaclouds/platform/dashboard/servlets/ListApplicationsServlet.java
@@ -2,8 +2,11 @@ package eu.seaclouds.platform.dashboard.servlets;
 
 import brooklyn.rest.client.BrooklynApi;
 import brooklyn.rest.domain.ApplicationSummary;
+import brooklyn.rest.domain.LocationSummary;
 import brooklyn.rest.domain.SensorSummary;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import eu.seaclouds.platform.dashboard.ConfigParameters;
 
 import javax.servlet.ServletException;
@@ -34,12 +37,49 @@ public class ListApplicationsServlet extends HttpServlet {
         });
 
 
+
+
         if (applicationSummaries == null){
             response.sendError(500, "Connection error: couldn't reach SeaClouds endpoint");
         }else{
+
+            JsonArray jsonResult = new JsonArray();
+
+            for (ApplicationSummary application : applicationSummaries) {
+
+                JsonObject jsonApplication = new JsonObject();
+                jsonResult.add(jsonApplication);
+
+                jsonApplication.addProperty("id", application.getId());
+                jsonApplication.addProperty("status", application.getStatus().toString());
+
+                JsonObject jsonSpec = new JsonObject();
+                jsonApplication.add("spec", jsonSpec);
+
+                jsonSpec.addProperty("name", application.getSpec().getName());
+                jsonSpec.addProperty("type", application.getSpec().getName());
+
+                JsonArray jsonArrayLocations = new JsonArray();
+                jsonSpec.add("locations", jsonArrayLocations);
+
+                for (String location : application.getSpec().getLocations()) {
+                    LocationSummary locationSumary = BROOKLKYN_API.getLocationApi().get(location, null);
+
+                    if(locationSumary != null){
+                        JsonObject jsonLocation = new JsonObject();
+                        jsonArrayLocations.add(jsonLocation);
+                        jsonLocation.addProperty("id", locationSumary.getId());
+                        jsonLocation.addProperty("name", locationSumary.getName());
+                        jsonLocation.addProperty("type", locationSumary.getType());
+                        jsonLocation.addProperty("spec", locationSumary.getSpec());
+                    }
+                }
+
+            }
+
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(new Gson().toJson(applicationSummaries));
+            response.getWriter().write(new Gson().toJson(jsonResult));
         }
     }
 }
