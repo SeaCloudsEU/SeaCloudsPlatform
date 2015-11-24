35ba793450e7a2d4363a7336b7a5092430fa463b
Simone Zenzaro <zenzaro@di.unipi.it>
2015-11-24 13:58:19 +0100

    Added more logging

--- planner/core/src/main/java/eu/seaclouds/platform/planner/core/Planner.java
+++ planner/core/src/main/java/eu/seaclouds/platform/planner/core/Planner.java
@@ -134,10 +134,20 @@ public class Planner {
 
     public String[] plan(List<String> deployableOfferings) throws ParsingException, IOException {
 
+        log.info("Planning for aam: \n" + aam);
+
         //Get offerings
         log.info("Getting Offeing Step: Start");
         Map<String, Pair<NodeTemplate, String>> offerings = getOfferings(deployableOfferings); // getOfferingsFromDiscoverer();
         log.info("Getting Offeing Step: Complete");
+
+        log.info("\nNot deployable offering have been filtered!");
+        log.info("\nDeployable offerings have location: " + deployableOfferings);
+
+        log.info("Got " + offerings.size() + " offerings from discoverer:");
+        for(String s: offerings.keySet()){
+            log.info("\n" + s + "\n\t" +offerings.get(s).second);
+        }
 
         //Matchmake
         log.info("Matchmaking Step: Start");
@@ -152,11 +162,15 @@ public class Planner {
             log.error("Error preparing matchmaker output for optimization", e);
         }
 
+        for(String s:matchingOfferings.keySet()){
+            log.info("Module " + s + "has matching offerings: " + matchingOfferings.get(s));
+        }
 
         log.info("Optimization Step: Start");
+        log.info("Calling optimizer with suitable offerings: \n" + mmOutput);
         Optimizer optimizer = new Optimizer();
         String[] outputPlans = optimizer.optimize(aam, mmOutput);
-
+        log.info("Optimzer result: " + Arrays.asList(outputPlans));
         log.info("Optimization Step: Complete");
 
         return outputPlans;
@@ -193,6 +207,7 @@ public class Planner {
         String[] outputPlans = optimizer.optimize(aam, mmOutput);
 
         log.info("Optimization Step: Complete");
+
         return  outputPlans;
     }
 
